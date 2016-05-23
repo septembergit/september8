405513d19f3adb60711d63147054f8bfd33303c6
septembergit <sarah_mailbox@163.com>
2016-05-23 17:20:05 +0800

    Update collect_all_even.js

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -3,11 +3,11 @@
 function collectAllEven (collection) {
 	var result = [],
 	    collectionStirng = collection.toString(),
-        i = 0;
-    collectionStirng.replace(/\d?[02468]/g, function($1) {
-    	result[i] = parseInt($1);
-    	i++;
-    }); 
+            i = 0;
+        collectionStirng.replace(/\d?[02468]/g, function($1) {
+        	result[i] = parseInt($1);
+        	i++;
+        }); 
 	return result;
 }
 
