02bf884c168d82b8f37bde20580b780865e2b461
septembergit <sarah_mailbox@163.com>
2016-05-23 17:16:48 +0800

    第八次作业

---
+++ src/collect_all_even.js
@@ -0,0 +1,14 @@
+'use strict';
+
+function collectAllEven (collection) {
+	var result = [],
+	    collectionStirng = collection.toString(),
+        i = 0;
+    collectionStirng.replace(/\d?[02468]/g, function($1) {
+    	result[i] = parseInt($1);
+    	i++;
+    }); 
+	return result;
+}
+
+module.exports = collectAllEven;
